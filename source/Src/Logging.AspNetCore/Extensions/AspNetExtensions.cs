546c0cbf39a5995779b4f03eddaca9e6ba345e24
Alessandro Chiavegato <alessandro.chiavegato@dgroove.it>
2019-06-25 17:09:19 +0200

    created extension package lib

---
+++ source/Src/Logging.AspNetCore/Extensions/AspNetExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Microsoft.Practices.EnterpriseLibrary.Logging.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for ASP.NET Core
+    /// </summary>
+    public static class AspNetExtensions
+    {
+        /// <summary>
+        /// Use Enterprise Library Logging for Dependency Injected loggers
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseEnterpriseLibraryLog(this IWebHostBuilder builder)
+        {
+            return UseEnterpriseLibraryLog(builder, () => null);
+        }
+
+        /// <summary>
+        /// Use Enterprise Library Logging for Dependency Injected loggers
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="optionsFunc">Builder options function for logging</param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseEnterpriseLibraryLog(this IWebHostBuilder builder, Func<LoggerOptions> optionsFunc)
+        {
+            return UseEnterpriseLibraryLog(builder, optionsFunc());
+        }
+
+        /// <summary>
+        /// Use Enterprise Library Logging for Dependency Injected loggers
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="options">Options for logging</param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseEnterpriseLibraryLog(this IWebHostBuilder builder, LoggerOptions options)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.ConfigureServices(services => ConfigureEnterpriseLibraryLog(options, services, serviceProvider => serviceProvider.GetService<IConfiguration>()));
+            return builder;
+        }
+
+        private static void ConfigureEnterpriseLibraryLog(LoggerOptions options, IServiceCollection services, Func<IServiceProvider, IConfiguration> lookupConfiguration)
+        {
+            services.AddSingleton<ILoggerProvider>(serviceProvider =>
+            {
+                LoggerProvider provider = new LoggerProvider(options ?? new LoggerOptions());
+                IConfiguration configuration = lookupConfiguration(serviceProvider);
+                return provider;
+            });
+        }
+    }
+}
---
+++ source/Src/Logging.AspNetCore/LoggerOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Practices.EnterpriseLibrary.Logging.AspNetCore
+{
+    /// <summary>
+    /// Options for logging
+    /// </summary>
+    public class LoggerOptions
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LoggerOptions()
+        {
+            
+        }
+
+        /// <summary>
+        /// Get or set custom configuration filepath. If empty default application config file is used.
+        /// </summary>
+        public string ConfigurationFilepath { get; set; }
+    }
+}
