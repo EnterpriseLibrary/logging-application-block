779e43c59e6c4fa24454da27bea8c8ecec5d6290
Artem Kliatchkine <eldog@rambler.ru>
2023-02-24 11:12:20 +0100

    EventLog trace listener functionality included in net6/7 builds.

--- source/Src/Logging/Configuration/Fluent/ILoggingConfigurationSendToEventLogTraceListener.cs
+++ source/Src/Logging/Configuration/Fluent/ILoggingConfigurationSendToEventLogTraceListener.cs
@@ -1,16 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
 using System.Diagnostics;
-using System.Messaging;
 using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
-using System.Collections.Specialized;
 using Microsoft.Practices.EnterpriseLibrary.Logging.Formatters;
-using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Fluent;
 
 namespace Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Fluent
 {
