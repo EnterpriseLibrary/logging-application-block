eaf8d3038b5f1f3d17cee973575a825f477243dd
Ali <Ali@DESKTOP-244LJMK>
2025-08-07 17:10:40 +0330

    remove ignore

--- source/Tests/Logging/TracerTransparencyFixture.cs
+++ source/Tests/Logging/TracerTransparencyFixture.cs
@@ -180,6 +180,7 @@ namespace Microsoft.Practices.EnterpriseLibrary.Logging.Tests.TracerTransparency
         }
 
         [TestClass]
+        [Ignore("Ignored in .NET 5+ because partial trust AppDomains are not supported.")]
         public class when_tracing_operation : given_log_writer_in_partial_trust_app_domain_with_unmanaged_code_permission
         {
             private IDictionary<string, string>[] entryProperties;
@@ -245,7 +246,7 @@ namespace Microsoft.Practices.EnterpriseLibrary.Logging.Tests.TracerTransparency
         }
 
         [TestClass]
-        [Ignore("Ignored in .NET 5+ because partial trust AppDomains are not supported.")]
+        
         public class when_tracing_operation_with_explicit_activity_id : given_log_writer_in_partial_trust_app_domain_with_unmanaged_code_permission
         {
             private IDictionary<string, string>[] entryProperties;
