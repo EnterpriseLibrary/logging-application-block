2d0b6316be593aaaf5d8920ffc806b1f1739086c
Ali <Ali@DESKTOP-244LJMK>
2025-11-15 01:45:15 +0330

    update setup

--- source/Tests/Logging/Distributor/LogDistributorFixture.cs
+++ source/Tests/Logging/Distributor/LogDistributorFixture.cs
@@ -32,8 +32,11 @@ namespace Microsoft.Practices.EnterpriseLibrary.Logging.Distributor.Tests
             AppDomain.CurrentDomain.SetData("APPBASE", Environment.CurrentDirectory);
 
             logWriter =
-
+#if NETCOREAPP || NET5_0_OR_GREATER
                 new LogWriterFactory(NetCoreHelper.LookupConfigSection).Create();
+#else
+               new LogWriterFactory().Create();
+#endif
 
             MockTraceListener.Reset();
             ErrorsMockTraceListener.Reset();
